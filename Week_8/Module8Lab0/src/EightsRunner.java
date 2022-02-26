4c0fca513540fecc7e5856d2a30507bce5984fa8
uzel <uzel@kabina.xyz>
2022-02-25 23:57:25 -0800

    Created Module8Lab00/

---
+++ Week_8/Module8Lab0/src/EightsRunner.java
@@ -0,0 +1,10 @@
+public class EightsRunner {
+
+    /**
+     * Creates the game and runs it.
+     */
+    public static void main(String[] args) {
+        Eights game = new Eights();
+        game.playGame();
+    }
+}
